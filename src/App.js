4ff1c1a1971be94c28b415b6e47be98a8600d368
AdityaSurve <surveaditya521@gmail.com>
2023-03-11 11:53:29 +0530

    adding landing and nav

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Landing from "./pages/Landing"
 import Footer from "./components/Footer";
 import Login from "./Login/signup/Login";
 import Signup from "./Login/signup/Signup";
 import Home from "./pages/Home";
-
 function App() {
   return (
     <div>
-     
       <Router>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="/login" element={<Login />}></Route>
-          <Route path="/navbar" element={<Navbar />}></Route>
+          <Route path="/explore" element={<Landing />}></Route>
           <Route path="/footer" element={<Footer />}></Route>
         </Routes>
       </Router>
